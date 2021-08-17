995dea765407ae6b3db57a8f18b6affb1f31d993
huynvn010193 <hpn010193@gmail.com>
2021-08-17 22:46:40 +0700

    13-Setup API User

--- src/models/common.ts
+++ src/models/common.ts
@@ -3,8 +3,15 @@ export interface PaginationParams {
   _page: number;
   _total: number;
 }
-
 export interface ListResponse<T> {
   data: T[];
   pagination: PaginationParams;
+}
+
+export interface ListParams {
+  _page: number;
+  _limit: number;
+  _sort: number;
+  _order: 'asc' | 'desc';
+  [key: string]: any;
 }
