7373c57852357f7d84708c40711bf829fc48d101
huynvn010193 <hpn010193@gmail.com>
2021-08-21 12:20:45 +0700

    Redux-saga: 21 pending code

--- src/components/Layout/Admin.tsx
+++ src/components/Layout/Admin.tsx
@@ -1,7 +1,24 @@
 import * as React from 'react';
+import { Box, makeStyles } from '@material-ui/core';
 
-export interface AdminLayoutProps {}
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'grid',
+    gridTemplateRows: 'auto 1fr',
+    gridTemplateColumns: '300px 1fr',
+    gridTemplateAreas: `"header header" "sidebar main"`,
+    minHeight: '100vh'
+  },
+  header: {},
+  sidebar: {},
+  main: {}
+}))
 
-export default function AdminLayout(props: AdminLayoutProps) {
-  return <div>AdminLayout</div>;
+export default function AdminLayout() {
+  const classes = useStyles();
+  return <Box className={classes.root}>
+    <Box className={classes.header}>HEADER</Box>
+    <Box className={classes.sidebar}>SIDEBAR</Box>
+    <Box className={classes.main}>MAIN</Box>
+  </Box>;
 }
