5656785bded7883f54a316e5f31e0e25b02de992
huynvn010193 <hpn010193@gmail.com>
2021-08-21 15:22:20 +0700

    21-Redux-saga: Admin Layout with CSS grid and Material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
-import { Button } from '@material-ui/core';
-import citiApi from 'api/cityApi';
 import { NotFound, PrivateRoute } from 'components/Common';
 import AdminLayout from 'components/Layout/Admin';
-import { authActions } from 'features/auth/authSlice';
 import LoginPage from 'features/auth/pages/LoginPage';
-import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 function App() {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    citiApi.getAll().then((response) => console.log(response));
-  }, []);
   return (
     <>
-      <Button variant='contained' color='primary' onClick={() => dispatch(authActions.logout())}>
-        Logut
-      </Button>
       <Switch>
         <Route path='/login'>
           <LoginPage />
--- src/components/Layout/Admin.tsx
+++ src/components/Layout/Admin.tsx
@@ -1,24 +1,41 @@
 import * as React from 'react';
 import { Box, makeStyles } from '@material-ui/core';
+import { Header, Sidebar } from 'components/Common';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'grid',
     gridTemplateRows: 'auto 1fr',
-    gridTemplateColumns: '300px 1fr',
+    gridTemplateColumns: '240px 1fr',
     gridTemplateAreas: `"header header" "sidebar main"`,
-    minHeight: '100vh'
+    minHeight: '100vh',
   },
-  header: {},
-  sidebar: {},
-  main: {}
-}))
+  header: {
+    gridArea: 'header',
+  },
+  sidebar: {
+    gridArea: 'sidebar',
+    borderRight: `1px solid ${theme.palette.divider}`,
+    backgroundColor: theme.palette.background.paper,
+  },
+  main: {
+    gridArea: 'main',
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(2, 3),
+  },
+}));
 
 export default function AdminLayout() {
   const classes = useStyles();
-  return <Box className={classes.root}>
-    <Box className={classes.header}>HEADER</Box>
-    <Box className={classes.sidebar}>SIDEBAR</Box>
-    <Box className={classes.main}>MAIN</Box>
-  </Box>;
+  return (
+    <Box className={classes.root}>
+      <Box className={classes.header}>
+        <Header />
+      </Box>
+      <Box className={classes.sidebar}>
+        <Sidebar />
+      </Box>
+      <Box className={classes.main}>MAIN</Box>
+    </Box>
+  );
 }
