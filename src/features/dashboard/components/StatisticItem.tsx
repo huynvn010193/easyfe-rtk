b3da696b47b860433d3a4c6c7bb211b9100dabdf
huynvn010193 <hpn010193@gmail.com>
2021-08-26 14:52:22 +0700

    Done Redux Saga: 26 - Làm giao diện cho Statistic Item với Material UI

--- src/features/dashboard/components/StatisticItem.tsx
+++ src/features/dashboard/components/StatisticItem.tsx
@@ -1,14 +1,20 @@
 import * as React from 'react';
-import { Paper, Box, Typography } from '@material-ui/core';
-import { makeStyles } from '@material-ui/styles';
+import { Paper, Box, Typography, makeStyles } from '@material-ui/core';
 export interface StatisticItemProps {
   icon: React.ReactElement;
   label: string;
-  value: string;
+  value: number;
 }
 
 const useStyles = makeStyles((theme) => ({
-  root: {}
+  root: {
+    display: 'flex',
+    flexFlow: 'row nowrap',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: theme.spacing(1, 2),
+    border: `1px solid ${theme.palette.divider}`,
+  },
 }));
 
 export default function StatisticItem({ icon, label, value }: StatisticItemProps) {
@@ -17,8 +23,10 @@ export default function StatisticItem({ icon, label, value }: StatisticItemProps
   return (
     <Paper className={classes.root}>
       <Box>{icon}</Box>
-      <Typography variant="h5">{value}</Typography>
-      <Typography variant="caption">{label}</Typography>
+      <Box>
+        <Typography variant='h5'>{value}</Typography>
+        <Typography variant='caption'>{label}</Typography>
+      </Box>
     </Paper>
   );
 }
--- src/features/dashboard/index.tsx
+++ src/features/dashboard/index.tsx
@@ -1,7 +1,9 @@
-import { Box, Grid } from '@material-ui/core';
+import { Box, Grid, makeStyles, LinearProgress } from '@material-ui/core';
+import { ChatBubble, ChatRounded, PeopleAlt } from '@material-ui/icons';
 import { useAppSelector } from 'app/hooks';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
+import StatisticItem from './components/StatisticItem';
 import {
   dashboardAction,
   selectDashboardLoading,
@@ -11,6 +13,18 @@ import {
   selectRankingByCityList,
 } from './dashboardSlice';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    position: 'relative',
+    paddingTop: theme.spacing(1),
+  },
+  loading: {
+    position: 'relative',
+    top: theme.spacing(-1),
+    widh: '100%',
+  },
+}));
+
 export default function Dashboard() {
   const dispatch = useDispatch();
   const loading = useAppSelector(selectDashboardLoading);
@@ -18,6 +32,7 @@ export default function Dashboard() {
   const highestStudentList = useAppSelector(selectHighestStudentList);
   const lowestStudentList = useAppSelector(selectLowestStudentList);
   const rankingByCityList = useAppSelector(selectRankingByCityList);
+  const classes = useStyles();
 
   console.log({
     loading,
@@ -30,11 +45,41 @@ export default function Dashboard() {
   useEffect(() => {
     dispatch(dashboardAction.fetchData());
   }, [dispatch]);
-  return <Box>
-    {/* Statistic Section */}
-    <Grid container>
-      <Grid item xs={12} md={6} lg={4} xl={3}>
+  return (
+    <Box className={classes.root}>
+      {/* loading */}
+      {loading && <LinearProgress className={classes.loading} />}
+      {/* Statistic Section */}
+      <Grid container spacing={3}>
+        <Grid item xs={12} md={6} lg={3}>
+          <StatisticItem
+            icon={<PeopleAlt fontSize='large' color='primary' />}
+            label='female'
+            value={statistics.maleCount}
+          />
+        </Grid>
+        <Grid item xs={12} md={6} lg={3}>
+          <StatisticItem
+            icon={<ChatRounded fontSize='large' color='primary' />}
+            label='female'
+            value={statistics.femaleCount}
+          />
+        </Grid>
+        <Grid item xs={12} md={6} lg={3}>
+          <StatisticItem
+            icon={<ChatBubble fontSize='large' color='primary' />}
+            label='mark >= 8'
+            value={statistics.highMarkCount}
+          />
+        </Grid>
+        <Grid item xs={12} md={6} lg={3}>
+          <StatisticItem
+            icon={<PeopleAlt fontSize='large' color='primary' />}
+            label='mark =< 5'
+            value={statistics.lowMarkCount}
+          />
+        </Grid>
       </Grid>
-    </Grid>
-  </Box>;
+    </Box>
+  );
 }
